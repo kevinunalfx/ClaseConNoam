9d975b69167c58af8ac8f275694973b0bb928be8
MALAG <MALAG@KGB>
2025-04-24 17:24:35 -0500

    4. Metodo Factorial

--- src/clase2connoam/Clase2conNoam.java
+++ src/clase2connoam/Clase2conNoam.java
@@ -5,20 +5,30 @@
  */
 package clase2connoam;
 
+import java.util.Scanner;
 /**
  *
  * @author MALAG
  */
 public class Clase2conNoam {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static int factorial(int n){
+    if (n == 0 | n == 1){
+        return 1;
+    }
+    // Caso Recursivo  n! = n * (n-1)!
+    return n * factorial(n-1);
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // 1. Hola Soy Kevin Colombiano
         // 2. Hola soy Noam
         // 3. Como te ha parecido colombia?
         // 4. Trucha con patacones
+        System.out.println("Ingrese el numero: ");
+        int numero = sc.nextInt();
+        int resultado = factorial(numero);
+        System.out.println("Factorial de " + numero + " es: " + resultado);
     }
     
 }
