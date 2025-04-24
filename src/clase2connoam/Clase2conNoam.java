4de3b00be5f83cabdd534140edb0f1bf927a3962
MALAG <MALAG@KGB>
2025-04-24 16:44:17 -0500

    Primer Mensaje

---
+++ src/clase2connoam/Clase2conNoam.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase2connoam;
+
+/**
+ *
+ * @author MALAG
+ */
+public class Clase2conNoam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 1. Hola Soy Kevin Colombiano
+    }
+    
+}
